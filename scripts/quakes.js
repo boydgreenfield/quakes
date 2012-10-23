180161cda887457ffc24190358e76db5fc16f1e1
boydgreenfield <boyd.greenfield@gmail.com>
2012-10-23 14:32:15 -0700

    Enable automatic updating of the loadTime

--- scripts/quakes.js
+++ scripts/quakes.js
@@ -316,8 +316,8 @@ function drawGlobe(id, windowDim, paddingDim, countriesJSON, earthQuakesJSON, us
       // Finally update the data load time
       var currentDate = new Date();
       var format = d3.time.format("%A %B %e, %Y."); //  at %H:%M:%S
-      // loadTime
-      //   .text(format(currentDate));
+      loadTime
+        .text(format(currentDate));
     });
 
     // Timer before selection changes focus
